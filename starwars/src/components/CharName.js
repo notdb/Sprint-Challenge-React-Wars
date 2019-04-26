061f6adc145ee035e0791882124b80aecc63fa87
Doug <me@douglasbowen.com>
2019-04-26 12:49:05 -0400

    basic styling

--- starwars/src/components/CharName.js
+++ starwars/src/components/CharName.js
@@ -3,7 +3,11 @@ import React from "react";
 function CharName(props) {
   return (
     <div>
-      <p>{props.char.name}</p>
+      <p className="name">{props.char.name}</p>
+      <ul>
+        <li>Birth Year: {props.char.birth_year}</li>
+        <li>Weight: {props.char.mass}KG</li>
+      </ul>
     </div>
   );
 }
